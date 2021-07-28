c2dd88f8a6207b6068c89fedf35da495d8a97527
ajaypatil2498 <62547013+ajaypatil2498@users.noreply.github.com>
2021-07-28 15:59:47 +0530

    Create PullDemo.java

---
+++ NewJavaGitProject/src/main/java/com/lti/demo/PullDemo.java
@@ -0,0 +1,11 @@
+package com.lti.demo;
+
+public class PullDemo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("this file is cretaed in github after main and newbranch merging");
+    System.out.println("After this try pulling this file in eclipse");
+	}
+
+}
