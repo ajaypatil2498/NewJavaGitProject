1d4eebb416f1c9da9a0effd77e41a92f946f85cc
ajaypatil2498 <ajaypatil.2498@gmail.com>
2021-07-28 15:11:53 +0530

    second commit

--- NewJavaGitProject/src/main/java/com/lti/demo/MyFile.java
+++ NewJavaGitProject/src/main/java/com/lti/demo/MyFile.java
@@ -1,5 +1,8 @@
+package com.lti.demo;
 public class MyFile {
 	
 	public static void main(String[] args) {
 		System.out.println("This file added in github direct to check how to pull in eclipse");
+		System.out.println("Tried pushing from eclipse");
 	}
+}
